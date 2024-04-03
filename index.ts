fed8576e96c326ba21ff96512d341da3fe90bf67
Christopher Vachon <cvachon@prometrix.ca>
2024-04-03 15:55:04 -0400

    Update reusable workflow

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
+const processArgs = process.argv;
+const passedArguments = processArgs.slice(2);
+
+const args: Record<string, string> = { "-t": "was not passed via -t" };
+
+for (let i = 0; i < passedArguments.length; i = i + 2) {
+    const key = passedArguments[i];
+    const value = passedArguments[i + 1];
+
+    args[key] = value;
+}
+
+console.log("");
 console.log("Hello via Bun!");
+console.log(`The Passed Tag is: ${args["-t"]}`);
+console.log("");
